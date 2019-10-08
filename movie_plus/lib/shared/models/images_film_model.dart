2643e62aa6b763a30137539c8219ec22831ab1d4
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-10-08 20:30:26 -0300

    Added ImagesFilm model

---
+++ movie_plus/lib/shared/models/images_film_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ImagesFilmModel {
+  String mainImage;
+  String image1;
+  String image2;
+  String image3;
+  String image4;
+
+  ImagesFilmModel({@required this.mainImage, @required this.image4, @required this.image3, @required this.image2, @required this.image1});
+}
