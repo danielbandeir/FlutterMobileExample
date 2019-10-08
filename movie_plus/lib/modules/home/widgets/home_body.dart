9f227413ec4bfc1dd513e0dd5ad5c9eca8344a3a
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-10-08 20:14:12 -0300

    Added HomeBody widget

---
+++ movie_plus/lib/modules/home/widgets/home_body.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeBody extends StatefulWidget {
+  HomeBody({Key key}) : super(key:key);
+
+  @override
+  _HomeBodyState createState() => _HomeBodyState();
+}
+
+class _HomeBodyState extends State<HomeBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        
+      ],
+    );
+  }
+}
