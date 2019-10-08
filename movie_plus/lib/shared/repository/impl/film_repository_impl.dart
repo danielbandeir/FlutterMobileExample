189c102c42c78367766ee280865f33aa6bfe9fdd
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-10-08 20:50:29 -0300

    Added FilmRepositoryImpl

---
+++ movie_plus/lib/shared/repository/impl/film_repository_impl.dart
@@ -0,0 +1,11 @@
+import 'package:movie_plus/shared/models/film_description_model.dart';
+import 'package:movie_plus/shared/repository/film_repository.dart';
+
+class FilmRepositoryImpl implements FilmRepository {
+  @override
+  Future<List<FilmDescriptionModel>> getAllFilms() {
+    // TODO: implement getAllFilms
+    return null;
+  }
+
+}
