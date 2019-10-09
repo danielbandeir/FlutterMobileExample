a7938a6260fe9d6ddb168195fdc85b16c308ff51
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-10-08 23:09:42 -0300

    Added some changes in home bloc

--- movie_plus/lib/modules/home/home_bloc.dart
+++ movie_plus/lib/modules/home/home_bloc.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:movie_plus/shared/models/film_description_model.dart';
 import 'package:movie_plus/shared/repository/film_repository.dart';
 
 class HomeBloc {
   final FilmRepository filmRepository;
 
   HomeBloc({@required this.filmRepository});
+
+  Future<List<FilmDescriptionModel>> getAllFilms() async {
+    return filmRepository.getAllFilms();
+  }
 }
