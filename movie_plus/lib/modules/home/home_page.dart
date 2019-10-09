2fd3ea4a7ae9ede0bc7a2ea0edab5230f3e71f1b
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-10-08 21:21:33 -0300

    Added HomePage

--- movie_plus/lib/modules/home/home_page.dart
+++ movie_plus/lib/modules/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movie_plus/modules/home/home_bloc.dart';
 import 'package:movie_plus/modules/home/widgets/home_body.dart';
+import 'package:movie_plus/shared/repository/impl/film_repository_impl.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key:key);
@@ -12,7 +13,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  HomeBloc homeBloc;
+  HomeBloc homeBloc = new HomeBloc(filmRepository: FilmRepositoryImpl());
 
   @override
   Widget build(BuildContext context) {
