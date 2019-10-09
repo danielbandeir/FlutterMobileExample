8c03c023bacf46dc47c7b6afb251c40d98b2eb71
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-10-08 21:21:22 -0300

    Added repository to HomeBloc

--- movie_plus/lib/modules/home/home_bloc.dart
+++ movie_plus/lib/modules/home/home_bloc.dart
@@ -1,3 +1,8 @@
-class HomeBloc {
+import 'package:flutter/material.dart';
+import 'package:movie_plus/shared/repository/film_repository.dart';
 
+class HomeBloc {
+  final FilmRepository filmRepository;
+
+  HomeBloc({@required this.filmRepository});
 }
