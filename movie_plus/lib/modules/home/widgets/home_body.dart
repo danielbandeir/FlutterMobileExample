e85858423343dcd046748fff8b23625e33343590
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-10-08 23:10:16 -0300

    Changed homeB\ODY

--- movie_plus/lib/modules/home/widgets/home_body.dart
+++ movie_plus/lib/modules/home/widgets/home_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movie_plus/modules/home/home_bloc.dart';
+import 'package:movie_plus/modules/home/widgets/home_background.dart';
+import 'package:movie_plus/shared/models/film_description_model.dart';
 
 class HomeBody extends StatefulWidget {
   HomeBloc homeBloc;
@@ -14,13 +16,17 @@ class _HomeBodyState extends State<HomeBody> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: null,
+      future: widget.homeBloc.getAllFilms(),
       builder: (context, snapshot) {
-        return Stack(
+        if(snapshot.hasData && snapshot.data != null) {
+          return Stack(
             children: <Widget>[
-
-            ]
-        );
+              HomeBackground(imageUrl: snapshot.data)
+            ],
+          );
+        } else {
+          return Center(child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.black)));
+        }
       },
     );
   }
